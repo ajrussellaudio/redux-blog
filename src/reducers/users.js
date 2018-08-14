724c3fdd86aa5314a6cbb1b4ed6743c076881fa6
Alan Russell <ajrussellaudio@gmail.com>
2018-08-14 12:34:09 +0100

    fix equals typo

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -2,7 +2,7 @@ import { CREATE_USER } from "../actionTypes";
 
 export default (state = [], action) => {
   const { type, ...user } = action;
-  if ((type = CREATE_USER)) {
+  if (type === CREATE_USER) {
     return [...state, user];
   }
   return state;
