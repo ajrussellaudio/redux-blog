be7d27a92748bc951c24f43d2a7929aa35a1a5f8
Alan Russell <ajrussellaudio@gmail.com>
2018-08-14 12:34:36 +0100

    add functions to index

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import { createStore } from "redux";
+import appReducer from "./reducers";
+
+import { createUser, createPost } from "./actions";
+
+let store = createStore(appReducer);
+
+console.log("initial state:", store.getState());
+
+const unsubscribe = store.subscribe(() => {
+  console.log("stage changed:", store.getState());
+});
+
+store.dispatch(createUser("alan", "Alan Russell"));
+store.dispatch(createUser("dan", "Daniel Bugl"));
+
+store.dispatch(
+  createPost("dan", {
+    title: "First Post",
+    text: "Hello! This is the first blog post.",
+    category: "welcome"
+  })
+);
+store.dispatch(
+  createPost("alan", {
+    title: "First Impressions",
+    text: "Hiya. This is dead good.",
+    category: "reviews"
+  })
+);
