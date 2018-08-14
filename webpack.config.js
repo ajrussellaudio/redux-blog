e3a1896ef0d5d4e21e7cac743cf1862f6bab1086
Alan Russell <ajrussellaudio@gmail.com>
2018-08-14 10:12:43 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve("dist"),
+    filename: "main.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: "babel-loader",
+        exclude: "/node_modules"
+      }
+    ]
+  }
+};
